53fbf1e42f53f975583a05893ff466d828b99762
Vandergard <vadim.terentev.26s@gmail.com>
2018-12-25 16:02:04 +0300

    Hibernate task 17

---
+++ ModuleHibernate/Task17/src/main/java/pojo/Person.java
@@ -0,0 +1,40 @@
+package pojo;
+
+import javax.persistence.*;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String name;
+    @Column
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
