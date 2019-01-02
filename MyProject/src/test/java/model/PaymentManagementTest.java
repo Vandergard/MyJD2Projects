72f3d60ef20c937f7022018314c590281a32aece
Vandergard <vadim.terentev.26s@gmail.com>
2019-01-02 17:02:04 +0300

    Add Project of course JD2 with class mapping

---
+++ MyProject/src/test/java/model/PaymentManagementTest.java
@@ -0,0 +1,33 @@
+//package model;
+//
+//import configuration.HibernateXMLConfTest;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.test.context.support.AnnotationConfigContextLoader;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import javax.servlet.http.HttpServletRequest;
+//
+//import static junit.framework.TestCase.assertTrue;
+//
+////@RunWith(SpringJUnit4ClassRunner.class)
+////@ContextConfiguration(
+////        classes = {HibernateXMLConfTest.class},
+////        loader = AnnotationConfigContextLoader.class)
+////@Transactional
+//public class PaymentManagementTest {
+////
+////    @MockBean
+////    com.fasterxml.jackson.databind.ObjectMapper objectMapper;
+////
+////    @MockBean
+////    HttpServletRequest httpServletRequest;
+//
+//    @Test
+//    public void step1Test(){
+//        assertTrue(true);
+//    }
+//}
